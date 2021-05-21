df5547376d7104fa522c0609b588f8cd340709a5
kmkim <kmkim@triphos.co.kr>
2021-05-21 17:42:40 +0900

    update: navigation bar

--- src/components/header/NavigationBar.js
+++ src/components/header/NavigationBar.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Container, Category, ListType, right, Item } from "./NavigationBar.style";
+import { Container, Category, ListType, right, Item, MoveButton } from "./NavigationBar.style";
 
 const NavigationBar = () => {
 	return (
 		<Container>
-			<Item>
+			<Item flexNum="3">
 				<Category>뉴스스탠드</Category>
 				<Category className="gray"> > 뉴스스탠드</Category>
 				<Category>MY 뉴스</Category>
 			</Item>
 
-			<right>
+			<Item flexNum="2">
 				<ListType >카드형</ListType>
 				<ListType>리스트형</ListType>
+			</Item>
 
-				<ListType > {"<"} </ListType>
-				<ListType> {">"} </ListType>
-			</right>
+			<Item flexNum="1" direction="reverse">
+				<MoveButton > {"<"} </MoveButton>
+				<MoveButton > {">"} </MoveButton>
+			</Item>
 
 		</Container>
 	);
--- src/components/header/NavigationBar.style.js
+++ src/components/header/NavigationBar.style.js
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-	width: 100%;
 	display: flex;
 	flex-direction: row;
 	margin: 10px;
 	padding: 5px 10px;
 `;
+
 const Item = styled.div`
-	flex: 1
+	${props => {
+		return `
+			flex: ${props.flexNum};
+			flex-direction: ${props.direction === "reverse" ? "row-reverse" : "row"};
+		`
+	}}
+	
+	display: flex;
 `;
 
 const Category = styled.div`
@@ -22,11 +29,14 @@ const Category = styled.div`
 `;
 
 const ListType = styled.div`
-	float: right;
 `;
+
+const MoveButton = styled.div`
+`;
+
 const right = styled.div`
 	float: right;
 `;
 
 
-export { Container, Category, ListType, right, Item};
+export { Container, Category, ListType, right, Item, MoveButton};
